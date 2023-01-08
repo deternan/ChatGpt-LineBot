4a178bfd263d9d90071bfb5179ff8369ae88a0c1
Chao-Hsuan Ke <phelpske.dev@gmail.com>
2023-01-08 16:25:47 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ chatgpt = ChatGPT()
 # domain root
 @app.route('/')
 def home():
-    return 'Hello, B'
+    return 'Hello, LineBot'
 
 @app.route("/webhook", methods=['POST'])
 def callback():
@@ -40,19 +40,6 @@ def handle_message(event):
     if event.message.type != "text":
         return
     
-#     if event.message.text == "啟動":
-#         working_status = True
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
-#         return
-
-#     if event.message.text == "安靜":
-#         working_status = False
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
-#         return
     
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
